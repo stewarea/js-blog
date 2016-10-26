d23217f44b8fd927afabe533ed41b9d50f4d6218
Evan Stewart and Martin Cartledge <evan+martin@>
2016-10-26 13:53:20 -0700

    add functionaltiy to post in new-post component

--- app/components/new-post.js
+++ app/components/new-post.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewPost: false,
+  actions: {
+    showForm() {
+      this.set('addNewPost', true);
+    },
+    saveNewPost() {
+      var params = {
+        title: this.get("title"),
+        author: this.get("author"),
+        image: this.get("image"),
+        body: this.get("body"),
+      };
+      this.set('addNewPost', false);
+      this.sendAction('saveNewPost', params);
+    }
+  }
 });
